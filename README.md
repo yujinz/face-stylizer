824372d23adf30b1f7a56fe8ca5388a05aa86e34
yujinz <yujinzh18@gmail.com>
2018-04-13 01:22:05 -0500

    initial commit. facial landmark for video

---
+++ README.md
@@ -0,0 +1,8 @@
+Dependencies:
+cv2, dlib (use https://pypi.python.org/simple/dlib/ to install on Windows), skimage, numpy
+
+Face landmark model:
+http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+
+Reference:
+face_landmark_detection.py: http://dlib.net/face_landmark_detection.py.html & https://www.pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
---
+++ facial_landmark_detection.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+from __future__ import division
+import dlib
+import cv2
+import numpy as np
+
+def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
+    global ratio
+    w, h = img.shape
+
+    if width is None and height is None:
+        return img
+    elif width is None:
+        ratio = height / h
+        width = int(w * ratio)
+        resized = cv2.resize(img, (height, width), interpolation)
+        return resized
+    else:
+        ratio = width / w
+        height = int(h * ratio)
+        resized = cv2.resize(img, (height, width), interpolation)
+        return resized
+
+def rect_to_bb(rect):
+    # take a bounding predicted by dlib and convert it
+    # to the format (x, y, w, h) as we would normally do
+    # with OpenCV
+    x = int(rect.left()/ratio)
+    y = int(rect.top()/ratio)
+    w = int(rect.right()/ratio) - x
+    h = int(rect.bottom()/ratio) - y
+ 
+    return (x, y, w, h)
+
+def shape_to_np(shape, dtype="int"):
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((68, 2), dtype=dtype)
+
+    # loop over the 68 facial landmarks and convert them
+    # to a 2-tuple of (x, y)-coordinates
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+
+    # return the list of (x, y)-coordinates
+    return coords
+
+camera = cv2.VideoCapture(0)
+
+predictor_path = 'shape_predictor_68_face_landmarks.dat'
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+while True:
+
+    ret, frame = camera.read()
+    if ret == False:
+        print('Failed to capture frame from camera. Check camera index in cv2.VideoCapture(0) \n')
+        break
+
+    frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_resized = resize(frame_grey, width=120)
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    rects = detector(frame_resized, 1)
+    for (i, rect) in enumerate(rects):
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy array
+        shape = predictor(frame_resized, rect)
+        shape = shape_to_np(shape)
+
+        # convert dlib's rectangle to a OpenCV-style bounding box
+        # [i.e., (x, y, w, h)], then draw the face bounding box
+        (x, y, w, h) = rect_to_bb(rect)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            cv2.circle(frame, (int(x/ratio), int(y/ratio)), 3, (255, 255, 255), -1)
+            
+    cv2.imshow("image", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        camera.release()
+        break
