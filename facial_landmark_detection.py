66c0907352a672d48a8acf7d16ec791b51271431
yujinz <yujinzh18@gmail.com>
2018-04-13 02:29:05 -0500

    facial landmarks for static images

--- facial_landmark_detection.py
+++ facial_landmark_detection.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 from __future__ import division
+from skimage import io
+import sys
+import os
 import dlib
 import cv2
+import glob
 import numpy as np
 
 def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
@@ -44,46 +48,68 @@ def shape_to_np(shape, dtype="int"):
     # return the list of (x, y)-coordinates
     return coords
 
-camera = cv2.VideoCapture(0)
-
-predictor_path = 'shape_predictor_68_face_landmarks.dat'
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(predictor_path)
-
-while True:
-
-    ret, frame = camera.read()
-    if ret == False:
-        print('Failed to capture frame from camera. Check camera index in cv2.VideoCapture(0) \n')
-        break
-
-    frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_resized = resize(frame_grey, width=120)
-
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
-    rects = detector(frame_resized, 1)
+def get_facial_landmark(rects):
     for (i, rect) in enumerate(rects):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            i, rect.left(), rect.top(), rect.right(), rect.bottom()))
         # determine the facial landmarks for the face region, then
         # convert the facial landmark (x, y)-coordinates to a NumPy array
-        shape = predictor(frame_resized, rect)
+        shape = predictor(img_resized, rect)
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+                                                  shape.part(1)))
+
+        # Draw the face landmarks on the screen.
+        win.add_overlay(shape)
         shape = shape_to_np(shape)
 
         # convert dlib's rectangle to a OpenCV-style bounding box
         # [i.e., (x, y, w, h)], then draw the face bounding box
         (x, y, w, h) = rect_to_bb(rect)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # loop over the (x, y)-coordinates for the facial landmarks
         # and draw them on the image
-        for (x, y) in shape:
-            cv2.circle(frame, (int(x/ratio), int(y/ratio)), 3, (255, 255, 255), -1)
-            
-    cv2.imshow("image", frame)
+        # for (x, y) in shape:
+        #     cv2.circle(img, (int(x/ratio), int(y/ratio)), 3, (255, 255, 255), -1)
+        #     print(str(int(x/ratio)) + " " + str(int(y/ratio)))        
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        camera.release()
-        break
+    return shape
+
+
+if len(sys.argv) != 2:
+    print(
+        "Give the directory containing the facial images.\n"
+        "e.g.:\n"
+        "    ./face_landmark_detection.py ./examples/faces\n")
+    exit()
+
+predictor_path = "shape_predictor_68_face_landmarks.dat"
+# You can download a trained facial shape predictor from:
+# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+faces_folder_path = sys.argv[1]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+win = dlib.image_window()
+
+for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
+    print("Processing file: {}".format(f))
+    img = io.imread(f)
+
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_resized = resize(img_grey, width=500)
+
+    win.clear_overlay()
+    win.set_image(img_resized)
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.    
+    rects = detector(img_resized, 1)
+
+    landmarks = get_facial_landmark(rects)
+
+    win.add_overlay(rects)
+
+    dlib.hit_enter_to_continue()
