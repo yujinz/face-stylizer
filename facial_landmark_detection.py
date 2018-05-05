1efbadd2cf285bc7d28578d14729880b90ea32b2
JohnsonSYK <as7807807@gmail.com>
2018-05-04 19:41:23 -0500

    extract coordinates in region and gaussian blur

--- facial_landmark_detection.py
+++ facial_landmark_detection.py
@@ -8,6 +8,7 @@ import cv2
 import glob
 import numpy as np
 
+
 def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
     global ratio
     h, w = img.shape[:2]
@@ -25,16 +26,18 @@ def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
         resized = cv2.resize(img, (width, height), interpolation)
         return resized
 
+
 def rect_to_bb(rect):
     # take a bounding predicted by dlib and convert it
     # to the format (x, y, w, h) as we would normally do
     # with OpenCV
-    x = int(rect.left()/ratio)
-    y = int(rect.top()/ratio)
-    w = int(rect.right()/ratio) - x
-    h = int(rect.bottom()/ratio) - y
- 
-    return (x, y, w, h)
+    x = int(rect.left() / ratio)
+    y = int(rect.top() / ratio)
+    w = int(rect.right() / ratio) - x
+    h = int(rect.bottom() / ratio) - y
+
+    return x, y, w, h
+
 
 def shape_to_np(shape, dtype="int"):
     # initialize the list of (x, y)-coordinates
@@ -48,7 +51,8 @@ def shape_to_np(shape, dtype="int"):
     # return the list of (x, y)-coordinates
     return coords
 
-def draw_and_write_landmark(img_resized, detector, predictor, outfilename, is_disp = False):
+
+def draw_and_write_landmark(img_resized, detector, predictor, outfilename, is_disp=False):
     if is_disp:
         win = dlib.image_window()
         win.clear_overlay()
@@ -83,42 +87,85 @@ def draw_and_write_landmark(img_resized, detector, predictor, outfilename, is_di
         # loop over the (x, y)-coordinates for the facial landmarks
         # and draw them on the image
         for (x, y) in shape:
-            #if i<43 or i>48:
+            # if i<43 or i>48:
             outfile.write("%d %d\r\n" % (int(x), int(y)))
         #     cv2.circle(img, (int(x/ratio), int(y/ratio)), 3, (255, 255, 255), -1)
 
-    outfile.close() 
+    outfile.close()
     if is_disp:
         win.add_overlay(rects)
         dlib.hit_enter_to_continue()
+    return shape
+
+
+def find_area(im, coords):
+    mask = np.zeros((im.shape[0], im.shape[1]))
+
+    cv2.fillConvexPoly(mask, coords, 1)
+    mask = mask.astype(np.bool)
+    out = np.zeros_like(im)
+    out[mask] = im[mask]
+
+    gauss_mask = mask.astype(np.float)
+    gauss_mask = cv2.GaussianBlur(gauss_mask, (5, 5), 0)
+    temp = []
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            if mask[i][j]:
+                temp.append([i, j])
+    all_coords = np.array(temp)
+    # print(all_coords)
+
+    #cv2.imshow("crop", out)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    return gauss_mask, all_coords
 
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-	    print(
-	        "Usage:\n"
-	        "    ./facial_landmark_detection.py ./examples/art/01.jpg ./examples/target/01.jpg\n")
-	    exit()
+    if len(sys.argv) != 3:
+        print(
+            "Usage:\n"
+            "    ./facial_landmark_detection.py ./examples/art/01.jpg ./examples/target/01.jpg\n")
+        exit()
 
-	predictor_path = "shape_predictor_68_face_landmarks.dat"
-	# You can download a trained facial shape predictor from:
-	# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-	art_face_path = sys.argv[1]
-	target_face_path = sys.argv[2]
+    predictor_path = "shape_predictor_68_face_landmarks.dat"
+    # You can download a trained facial shape predictor from:
+    # http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+    art_face_path = sys.argv[1]
+    target_face_path = sys.argv[2]
 
-	detector = dlib.get_frontal_face_detector()
-	predictor = dlib.shape_predictor(predictor_path)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(predictor_path)
 
-	#win = dlib.image_window()
+    # win = dlib.image_window()
 
-	print("Processing file: {}".format(art_face_path))
-	img = io.imread(art_face_path)
-	h, w, c = img.shape
-	img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	draw_and_write_landmark(img_grey, detector, predictor, "art")
+    print("Processing file: {}".format(art_face_path))
+    img = io.imread(art_face_path)
+    h, w, c = img.shape
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    vertices = draw_and_write_landmark(img_grey, detector, predictor, "art")
 
-	print("Processing file: {}".format(target_face_path))
-	img = io.imread(target_face_path)
-	img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	img_resized = resize(img_grey, width=w)
-	draw_and_write_landmark(img_resized, detector, predictor, "target")
+    print("Processing file: {}".format(target_face_path))
+    img = io.imread(target_face_path)
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_resized = resize(img_grey, width=w)
+    draw_and_write_landmark(img_resized, detector, predictor, "target")
+
+    left_eye = vertices[36:42]
+    print(left_eye)
+
+    img = io.imread(art_face_path)
+    cv2.imshow("test", img)
+    mask, left_eye_all = find_area(img, left_eye)
+
+    # print(len(left_eye_all))
+    for i in range(len(left_eye_all)):
+        img[left_eye_all[i][0]][left_eye_all[i][1]] = 255
+
+    #print(mask[73])
+
+    cv2.imshow("test", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -7,10 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from facial_landmark_detection import (resize, draw_and_write_landmark)
 from moving_least_squares import (mls_affine_deformation, mls_affine_deformation_inv,
-                       mls_similarity_deformation, mls_similarity_deformation_inv,
-                       mls_rigid_deformation, mls_rigid_deformation_inv)
-
-
+                                  mls_similarity_deformation, mls_similarity_deformation_inv,
+                                  mls_rigid_deformation, mls_rigid_deformation_inv)
 
 if len(sys.argv) != 3:
     print(
@@ -29,47 +27,46 @@ art_img = io.imread(art_face_path)
 tar_img = io.imread(target_face_path)
 ah, aw = art_img.shape[:2]
 th, tw = tar_img.shape[:2]
-#print(art_img.shape[:2], tar_img.shape[:2], th*1.0/tw, ah*1.0/aw)
-is_height_resize = th*1.0/tw > ah*1.0/aw
+# print(art_img.shape[:2], tar_img.shape[:2], th*1.0/tw, ah*1.0/aw)
+is_height_resize = th * 1.0 / tw > ah * 1.0 / aw
 if is_height_resize:
-	print("resize height")
-	art_resized = resize(art_img, height=500)
-	tar_resized = resize(tar_img, height=500)
+    print("resize height")
+    art_resized = resize(art_img, height=500)
+    tar_resized = resize(tar_img, height=500)
 else:
-	art_resized = resize(art_img, width=300)
-	tar_resized = resize(tar_img, width=300)
+    art_resized = resize(art_img, width=300)
+    tar_resized = resize(tar_img, width=300)
 art_grey = cv2.cvtColor(art_resized, cv2.COLOR_BGR2GRAY)
 tar_grey = cv2.cvtColor(tar_resized, cv2.COLOR_BGR2GRAY)
-draw_and_write_landmark(art_grey, detector, predictor, "art", True)#, True
+draw_and_write_landmark(art_grey, detector, predictor, "art", True)  # , True
 draw_and_write_landmark(tar_grey, detector, predictor, "target")
 
 p = np.loadtxt('output/art.txt')
 q = np.loadtxt('output/target.txt')
 if is_height_resize:
-	p = p/2
-	q = q/2
-	art_resized = resize(art_img, height=250)
+    p = p / 2
+    q = q / 2
+    art_resized = resize(art_img, height=250)
 
 art_transformed = mls_rigid_deformation_inv(art_resized, p, q, alpha=1, density=1)
 scipy.misc.imsave('output/temp.jpg', art_transformed)
 if is_height_resize:
-	art_transformed = resize(art_transformed, height=500)
-#plt.imshow(art_transformed)
-#plt.show()
+    art_transformed = resize(art_transformed, height=500)
+# plt.imshow(art_transformed)
+# plt.show()
 
 im1 = art_transformed
 im2 = cv2.normalize(tar_resized, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        
+
 height1, width1 = im1.shape[:2]
 height2, width2 = im2.shape[:2]
 result = im2
 for y in range(height2):
     for x in range(width2):
-    	if y < height1 and x < width1:
-	        result[y, x, 0] = result[y, x, 0] / 2 + im1[y, x, 0] / 2
-	        result[y, x, 1] = result[y, x, 1] / 2 + im1[y, x, 1] / 2
-	        result[y, x, 2] = result[y, x, 2] / 2 + im1[y, x, 2] / 2
-
+        if y < height1 and x < width1:
+            result[y, x, 0] = result[y, x, 0] / 2 + im1[y, x, 0] / 2
+            result[y, x, 1] = result[y, x, 1] / 2 + im1[y, x, 1] / 2
+            result[y, x, 2] = result[y, x, 2] / 2 + im1[y, x, 2] / 2
 
 plt.imshow(result)
 plt.show()
